d70ed5f1ed7931c1a70228df6617cf107cea05cb
CassianoSocorro <cassianosocorro22@gmail.com>
2026-05-10 20:58:14 -0300

    Estrutura do projeto 2.0

---
+++ backend/src/controllers/usuariosController.ts
@@ -0,0 +1,20 @@
+[
+  {
+    "id": 1,
+    "nome": "Patrick organizador",
+    "email": "patrickturia@gmail.com",
+    // "senha": ".",
+    "tipo": "admin",
+    "zona": null,
+    "ativo": true
+  },
+  {
+    "id": 2,
+    "nome": "Carlos",
+    "email": "carlos@smartcup.com",
+    "senha": "...",
+    "tipo": "garcom",
+    "zona": "A",
+    "ativo": true
+  }
+]
