04d3a01cb8621a63e7340a6e92cbe1d584dd1266
Rossandre <rossandrefilho4@gmail.com>
2026-06-13 06:14:05 -0300

    pequena correção do tamanho da foto

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,8 @@ import smartcupsRoutes from "./routes/smartcupsRoutes.js";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(authMiddleware);
 
 app.use("/usuarios", usuariosRoutes);
